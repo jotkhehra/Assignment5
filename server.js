ea4d228a6ef1c68cfd42129fb52d8414e5d16e5a
jotkhehra <137837524+jotkhehra@users.noreply.github.com>
2023-12-09 01:44:52 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const pageRoutes = require("./routes/page.routes");
+const apiRoutes = require("./routes/api.routes");
+const {connect, sync} = require("./db");
+
+const app = express();
+const port = 4500;
+
+//SET THE VIEW ENGINE
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use(express.json());
+
+//Database stuff
+ connect();
+ sync();
+
+// ROUTE HANDLING
+app.use(pageRoutes);
+app.use("/api", apiRoutes);
+
+
+app.listen(port,()=>{
+console.log(`Listening on port ${port}`);
+});
+
+
+
+// LISTEN FOR REQUESTS!!!!
+
